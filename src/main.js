7b1d5a17ef6a0f962377a8700f4d3ccf8f04d105
martyumlas <martyumlas@gmail.com>
2025-01-15 21:18:27 +0800

    initial functionalities

--- src/main.js
+++ src/main.js
@@ -1,1 +1,13 @@
-console.log(document.querySelector('.message').textContent)
+
+const guess = document.querySelector('.guess')
+const message = document.querySelector('.message')
+const random = Math.floor(Math.random() * 20) + 1;
+
+const check = () => {
+    console.log(random)
+    if (Number(guess.value) !== random) {
+        alert('You failed')
+    } else {
+        alert('fuck')
+    }
+}
