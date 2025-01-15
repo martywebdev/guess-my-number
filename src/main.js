c96c2a1f46a4dba3e669f4fe8ba1f429f2e9b702
martyumlas <martyumlas@gmail.com>
2025-01-15 21:31:33 +0800

    initial functionalities

--- src/main.js
+++ src/main.js
@@ -1,13 +1,28 @@
 
 const guess = document.querySelector('.guess')
 const message = document.querySelector('.message')
-const random = Math.floor(Math.random() * 20) + 1;
+let  random;
+const number = document.querySelector('.number')
+
+guess.addEventListener('change', e => {
+    number.textContent = e.target.value
+})
 
 const check = () => {
     console.log(random)
-    if (Number(guess.value) !== random) {
-        alert('You failed')
-    } else {
-        alert('fuck')
+    if (Number(guess.value) < random) {
+        message.textContent = 'Higher'
+    } else if (Number(guess.value) > random) {
+        message.textContent = 'Lower'
+    } else if (Number(guess.value) === random) {
+        message.textContent = '🎉Correct Number'
     }
 }
+
+const reset = () => {
+    guess.value = ''
+    number.textContent = '?'
+    random =  Math.floor(Math.random() * 20) + 1;
+}
+
+reset()
