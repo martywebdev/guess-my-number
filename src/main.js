1b314454466f1c801c3794af67d82baea02d90d0
martyumlas <martyumlas@gmail.com>
2025-01-18 08:48:40 +0800

    add scoring and restrictions

--- src/main.js
+++ src/main.js
@@ -4,9 +4,10 @@ const message = document.querySelector('.message')
 const number = document.querySelector('.number')
 const checkBtn = document.querySelector('.check');
 const resetBtn = document.querySelector('.again');
+const score = document.querySelector('.score')
 
 let  random;
-
+let userScore = Number(score.textContent)
 
 guess.addEventListener('input', e => {
     number.textContent = e.target.value
@@ -15,24 +16,36 @@ guess.addEventListener('input', e => {
 const check = () => {
     console.log(random);  // For debugging purposes
     const userGuess = Number(guess.value);
+
+    if (userScore < 1) {
+        message.textContent = 'You lose!'
+        return
+    }
+    
     if (!userGuess) {
         message.textContent = '🎈No Number';
+        userScore--
     } else {
         if (userGuess < random) {
-            message.textContent = 'Higher';
+            message.textContent = 'Too Low';
+            userScore--
         } else if (userGuess > random) {
-            message.textContent = 'Lower';
+            message.textContent = 'Too high';  
+            userScore--
         } else if (userGuess === random) {
             message.textContent = '🎉Correct Number';
+            //add score
         }
     }
     
+    score.textContent = userScore
 };
 
 const reset = () => {
     guess.value = '';
     number.textContent = '?';
     message.textContent = '';
+    score.textContent = '20'
     random = Math.floor(Math.random() * 20) + 1;
 }
 
